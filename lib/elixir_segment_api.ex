a8740ac0cfdba0b0f8dccf8b3384354dcc75fdcf
Jason Freeman <jason.freeman@pixelunion.net>
2019-02-05 12:21:30 -0800

    Remove PixelBoard-specific event function

--- lib/elixir_segment_api.ex
+++ lib/elixir_segment_api.ex
@@ -23,16 +23,10 @@ defmodule SegmentAPI do
     end
   end
 
-  def event(app_slug, type), do: "#{app_slug} #{get_event_text(type)}"
-
   def process_response_status_code(200), do: Logger.debug("#{__MODULE__} successfully called")
 
   def process_response_status_code(status_code),
     do: Logger.info("#{__MODULE__} not successfully called, returned #{status_code}")
 
   defp headers, do: [Authorization: @auth_header, "Content-Type": "application/json"]
-
-  defp get_event_text(:customer_redact), do: "Customer Data Removal Requested"
-  defp get_event_text(:customer_data_request), do: "Customer Data Requested"
-  defp get_event_text(:shop_redact), do: "Data Removal Requested"
 end
